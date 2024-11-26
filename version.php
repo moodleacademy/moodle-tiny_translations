2a6d5b464a0853a71d3b027035b8f1c108bb020c
Rajneel Totaram <rjnlfj@yahoo.com>
2024-11-26 16:03:55 +1200

    Bump version for new release

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_translations';
 $plugin->release = '1.0.0';
-$plugin->version = 2024050100;
+$plugin->version = 2024050101;
 $plugin->requires = 2023100900; // Moodle 4.3 or newer.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = ['filter_translations' => 2024050100];
